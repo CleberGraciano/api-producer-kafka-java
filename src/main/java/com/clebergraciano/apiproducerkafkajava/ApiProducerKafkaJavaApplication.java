8908ba091470e5a56fb2f79c4fe1d8ed9f35b82b
Cleber <cleber_batista2010@hotmail.com>
2022-06-18 14:52:56 -0300

    Aplicação producer Kafka

---
+++ src/main/java/com/clebergraciano/apiproducerkafkajava/ApiProducerKafkaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.clebergraciano.apiproducerkafkajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProducerKafkaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProducerKafkaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clebergraciano/apiproducerkafkajava/ApiProducerKafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clebergraciano.apiproducerkafkajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProducerKafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
